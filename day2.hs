72c3f66aab78dddedfbe006fabc43a181db24b73
Akash Kurdekar <akash.kurdekar@sedex.com>
2024-12-02 14:06:01 +0000

    refactor day2

--- day2.hs
+++ day2.hs
@@ -1,15 +1,44 @@
-import Data.List (reverse, sort, splitAt)
+import Data.List
+import Data.Ord
 
-parse :: String -> [[Int]]
-parse input =
-  map (map readInt) (map words $ lines input)
+day2part1 :: [[Int]] -> Int
+day2part1 input =
+  length $ filter isSafe input
 
--- print . map readInt . words $ contents
+day2part2 :: [[Int]] -> Int
+day2part2 input =
+  length $ filter (\report -> isSafe report || isSafeByRemovingAny report) input
 
--- alternately, main = print . map readInt . words =<< readFile "test.txt"
+isSafe :: [Int] -> Bool
+isSafe report =
+  isAscOrDesc report && distancesOk report
+
+isAscOrDesc :: [Int] -> Bool
+isAscOrDesc report =
+  sort report == report || sortBy (comparing Data.Ord.Down) report == report
+
+distancesOk :: [Int] -> Bool
+distancesOk report =
+  case report of
+    [] -> True
+    [x] -> True
+    [x, y] -> abs (y - x) >= 1 && abs (y - x) <= 3
+    x : y : xs -> distancesOk [x, y] && distancesOk (y : xs)
+
+getShorterLists :: [Int] -> [[Int]]
+getShorterLists report =
+  map (\i -> (fst (splitAt i report)) ++ (tail $ snd (splitAt i report))) [0 .. (length report) - 1]
+
+isSafeByRemovingAny :: [Int] -> Bool
+isSafeByRemovingAny report =
+  any isSafe $ getShorterLists report
 
 readInt :: String -> Int
 readInt = read
+
+parse :: String -> [[Int]]
+parse input =
+  map (map readInt . words) (lines input)
 
 sample :: [[Int]]
 sample =
@@ -20,48 +49,6 @@ sample =
     [8, 6, 4, 4, 1],
     [1, 3, 6, 7, 9]
   ]
-
-pairs [] = []
-pairs [x] = []
-pairs (x : y : rest) = (x, y) : pairs (y : rest)
-
-isAscOrDesc report =
-  ascSortedReported == report || descSortedReport == report
-  where
-    ascSortedReported = sort report
-    descSortedReport = reverse $ sort $ report
-
-distancesOk :: [Int] -> Bool
-distancesOk report =
-  case report of
-    [] -> True
-    [x] -> True
-    [x, y] -> abs (y - x) >= 1 && abs (y - x) <= 3
-    x : xs -> abs (x - (head xs)) >= 1 && abs (x - head xs) <= 3 && distancesOk xs
-
-isSafe report =
-  isAscOrDesc report && distancesOk report
-
-getShorterLists :: [Int] -> [[Int]]
-getShorterLists report =
-  map (\i -> (fst (splitAt i report)) ++ (tail $ snd (splitAt i report))) [0 .. (length report) - 1]
-
--- map (\i -> report !! i) [0 .. (length report) - 1]
-
-isSafeByRemovingAny :: [Int] -> Bool
-isSafeByRemovingAny report =
-  any isSafe $ getShorterLists report
-
-isSafeWithDampener :: [Int] -> Bool
-isSafeWithDampener report =
-  isSafe report || (isSafeByRemovingAny report)
-
-day2part1 input =
-  length $ filter isSafe input
-
-day2part2 :: [[Int]] -> Int
-day2part2 input =
-  length $ filter isSafeWithDampener input
 
 main :: IO ()
 main = do
